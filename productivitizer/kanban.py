fcbfeff9b2650124a1df1801dcbad4d055560369
Bakhtiyar <sagidanovbakhtiyar@gmail.com>
2024-02-28 07:16:58 +0500

    Kanban api endpoints p1

--- productivitizer/kanban.py
+++ productivitizer/kanban.py
@@ -1,8 +1,48 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, g, jsonify, request
+from werkzeug.exceptions import abort
+from productivitizer.auth import login_required
+from productivitizer.db import get_db
+
 
 bp = Blueprint('kanban', __name__)
 
 
-@bp.route('/kanban')
-def kanban():
-    return render_template('kanban/kanban.html')
+# Kanban api implementation
+# Endpoint to get all tasks for current user
+@bp.route('/api/kanban/tasks', methods=['GET'])
+@login_required
+def get_task():
+    db = get_db()
+    tasks = db.execute(
+        'SELECT id, task_title, task_description, task_status'
+        ' FROM kanban WHERE user_id = ?', (g.user['id'],)
+    ).fetchall()
+    return jsonify(tasks), 200
+
+
+# Endpoint to create a new task for current user
+@bp.route('/api/kanban/tasks', methods=['POST'])
+@login_required
+def create_task():
+
+    # Get data from user input
+    data = request.json
+    task_title = data.get('task_title')
+    task_description = data.get('task_description')
+    task_status = data.get('task_status', 'todo')
+
+
+    # Validate title input of user
+    if not task_title:
+        return jsonify({'error': 'Task title is required'}), 400
+    
+
+    # Write new task into database
+    db = get_db()
+    db.execute(
+        'INSERT INTO kanban (user_id, task_title, task_description, task_status)'
+        ' VALUES (?, ?, ?, ?)', (g.user['id'], task_title, task_description, task_status)
+    )
+    db.commit()
+    return jsonify({'message': 'Task created successfully'}), 201
+    
